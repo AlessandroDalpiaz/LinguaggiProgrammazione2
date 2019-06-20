04e8dd1810a1e7875fdf00435d9ca11b0b53dfc9
AlessandroDalpiaz <alessandro.dalpiaz@gmail.com>
2019-06-20 15:44:12 +0200

    Upade change color

--- NetBeansJava_lab/EsempioGraficoCarte/src/CartaFX.java
+++ NetBeansJava_lab/EsempioGraficoCarte/src/CartaFX.java
@@ -32,21 +32,35 @@ public class CartaFX extends StackPane {
         public void handle(MouseEvent e) { 
             System.out.println("CLICCATO"); 
             System.out.println(e.getSource());
+            if(rett.getFill()==Color.TRANSPARENT){
+                System.out.println("entro");
+                String s=e.getSource().toString().split(" ")[1];
+                System.out.println(s);
+                switch(s){
+                    case "C":rett.setFill(Color.TOMATO);System.out.println("entroo");break;
+                    case "P":rett.setFill(Color.VIOLET);break;
+                    case "Q":rett.setFill(Color.BLANCHEDALMOND);break;
+                    case "F":rett.setFill(Color.GREENYELLOW);break;
+                }
+                t.setFill(Color.BLACK);
+            }else{
             rett.setFill(Color.TRANSPARENT);
-            t.setText(null);
-            } 
+            t.setFill(Color.TRANSPARENT);
+            }} 
         };    
     
     
     CartaFX(int j,int i) {
        n=j;
        seme= scelta(i);
-       switch(i){
+       /*switch(i){
            case 0:rett.setFill(Color.RED);break;
            case 1:rett.setFill(Color.VIOLET);break;
            case 2:rett.setFill(Color.BLANCHEDALMOND);break;
            case 3:rett.setFill(Color.GREENYELLOW);break;
-       }
+       }*/
+       rett.setFill(Color.TRANSPARENT);
+       t.setFill(Color.TRANSPARENT);
        
        rett.setStroke(Color.BLACK);
        
