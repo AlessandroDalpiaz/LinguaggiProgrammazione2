a513be6712cc4002155daf642ff33ca281d1c823
AlessandroDalpiaz <alessandro.dalpiaz@gmail.com>
2019-06-24 11:55:45 +0200

    Fix calculator

--- NetBeansJava_lab/it.unitn.dalpiaz.calcolatrice2/src/Calcolatrice2.java
+++ NetBeansJava_lab/it.unitn.dalpiaz.calcolatrice2/src/Calcolatrice2.java
@@ -4,21 +4,29 @@
  * and open the template in the editor.
  */
 
+import com.sun.scenario.effect.Effect;
 import java.awt.geom.Path2D;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.ColorInput;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.stage.Modality;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 /**
@@ -26,12 +34,14 @@ import javafx.stage.Stage;
  * @author Alessandro
  */
 public class Calcolatrice2 extends Application {
+    Stage mainWindow = null;
     TextField input1=new TextField();
     TextField input2=new TextField();
     TextField output=new TextField();
 
     public Calcolatrice2() {
         output.setDisable(true);
+        
     }
     
     
@@ -40,6 +50,7 @@ public class Calcolatrice2 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        mainWindow = primaryStage;
         BorderPane root =new BorderPane();
         final TilePane campi=new TilePane();
         final TilePane OpBottons= new TilePane();
@@ -50,12 +61,13 @@ public class Calcolatrice2 extends Application {
         root.setTop(lt);
         BorderPane.setAlignment(lt, Pos.CENTER);
         //sx
-        Label text =new Label("|-|-|-|-|");
+        Label text =new Label("              ");
         root.setLeft(text);
+        root.setAlignment(text, Pos.CENTER);
         //----primo,secondo  e terzo box
         
         campi.setPrefColumns(1);
-        campi.getChildren().addAll(input1,input2,output);
+        
         
         //----bottoni operazioni----------
         OperationBotton btn_piu= new OperationBotton(this,"PIU", "+");
@@ -64,10 +76,39 @@ public class Calcolatrice2 extends Application {
         OperationBotton btn_diviso= new OperationBotton(this,"DIVISO", "/");
         
         OpBottons.getChildren().addAll(btn_piu,btn_meno,btn_per,btn_diviso);
-        
-        allbtn.getChildren().addAll(campi,OpBottons);
+        OpBottons.setAlignment(Pos.CENTER);
+        campi.getChildren().addAll(input1,input2,OpBottons,output);
+        allbtn.getChildren().addAll(campi);
         root.setCenter(allbtn);
-        Scene scene = new Scene(root, 1000, 700);
+        
+        //---------------right
+        Button btnClear=new Button("CLEAR");
+        EventHandler<MouseEvent> pulisci=new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent t) {
+            //DropShadow shadow = new DropShadow();
+                
+                input1.setText("");
+                input2.setText("");
+                output.setText("");
+                
+                //OpBottons.getChildren().get(0).setEffect(shadow);
+            }
+        };
+        /*//Removing the shadow when the mouse cursor is off
+        btnClear.addEventHandler(MouseEvent.MOUSE_EXITED, 
+            new EventHandler<MouseEvent>() {
+                @Override public void handle(MouseEvent e) {
+                    OpBottons.getChildren().get(0).setEffect(null);
+                }
+        });
+        
+        *////////////////////
+        btnClear.addEventFilter(MouseEvent.MOUSE_CLICKED, pulisci);
+        root.setRight(btnClear);
+        root.setAlignment(btnClear, Pos.CENTER);
+        Scene scene = new Scene(root, 600, 200);
         
         primaryStage.setTitle("MiniCalculator");
         primaryStage.setScene(scene);
@@ -90,6 +131,7 @@ public class Calcolatrice2 extends Application {
     public String somma(String s1,String s2){
         if(!checkTF(input1) || !checkTF(input2)){
             return "errore";
+            
         }
         String res;
         int in1=Integer.parseInt(s1);
@@ -105,7 +147,8 @@ public class Calcolatrice2 extends Application {
         d2=Double.parseDouble(input2.getText());
         return String.valueOf(d1+d2);
         }catch(Exception e){
-            System.out.println("eroore");
+            System.out.println("errore");
+            showPopup("Errore: valori non corretti");
         }
         return null;    
     }
@@ -116,7 +159,8 @@ public class Calcolatrice2 extends Application {
         d2=Double.parseDouble(input2.getText());
         return String.valueOf(d1-d2);
         }catch(Exception e){
-            System.out.println("eroore");
+            System.out.println("errore");
+            showPopup("Errore: valori non corretti");
         }
         return null;    
     }
@@ -127,7 +171,8 @@ public class Calcolatrice2 extends Application {
         d2=Double.parseDouble(input2.getText());
         return String.valueOf(d1*d2);
         }catch(Exception e){
-            System.out.println("eroore");
+            System.out.println("errore");
+            showPopup("Errore: valori non corretti");
         }
         return null;    
     }
@@ -138,7 +183,8 @@ public class Calcolatrice2 extends Application {
         d2=Double.parseDouble(input2.getText());
         return String.valueOf(d1/d2);
         }catch(Exception e){
-            System.out.println("eroore");
+            System.out.println("errore");
+            showPopup("Errore: valori non corretti");
         }
         return null;    
     }
@@ -150,6 +196,20 @@ public class Calcolatrice2 extends Application {
             case "*": output.setText(perd(input1.getText(),input2.getText()));break;
             case "/": output.setText(divd(input1.getText(),input2.getText()));break;
         }
+    }
+    public void showPopup(String msg){
+        Label label = new Label(msg);
+        label.setAlignment(Pos.CENTER);
+        label.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        Scene sc = new Scene(label, 500, 200);
+        Stage stage = new Stage();
+        stage.setScene(sc);
+        stage.setX(100);
+        stage.setY(100);
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(mainWindow);
+        stage.show();
+ 
     }
     /**
      * @param args the command line arguments
